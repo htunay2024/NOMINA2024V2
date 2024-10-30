f5d5b7cbfcbb2647903c5134671d745927b20a11
htunay2024 <harvytunay2020@gmail.com>
2024-10-30 15:50:46 -0600

    Update and rename FamiliarODB.php to Familiar_C.php

--- Controller/Familiar_C.php
+++ Controller/Familiar_C.php
@@ -3,7 +3,7 @@
 require_once 'SQLSRVConnector.php';
 require_once '../Model/Familiar.php';
 
-class FamiliarODB {
+class Familiar_C {
     private $connection;
 
     public function __construct() {
