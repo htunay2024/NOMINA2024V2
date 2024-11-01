5692998b0008fda6efea8fcad755a5ec123e1ed5
htunay2024 <harvytunay2020@gmail.com>
2024-11-01 17:39:46 -0600

    Update Familiar.php

--- Model/Familiar.php
+++ Model/Familiar.php
@@ -1,64 +1,25 @@
 <?php
 class Familiar {
-    private $idFamiliar;
-    private $nombre;
-    private $apellido;
-    private $relacion;
-    private $fechaNacimiento;
-    private $idEmpleado;
-    private $NombreCompleto;
+    private $idFamiliar, $nombre, $apellido, $relacion, $fechaNacimiento, $idEmpleado, $NombreCompleto;
 
     public function __construct($idFamiliar = null, $nombre, $apellido, $relacion, $fechaNacimiento, $idEmpleado, $NombreCompleto = null) {
-        $this->idFamiliar = $idFamiliar;
-        $this->nombre = $nombre;
-        $this->apellido = $apellido;
-        $this->relacion = $relacion;
-        $this->fechaNacimiento = $fechaNacimiento;
-        $this->idEmpleado = $idEmpleado;
-        $this->NombreCompleto = $NombreCompleto;
+        $this->idFamiliar = $idFamiliar; $this->nombre = $nombre; $this->apellido = $apellido; $this->relacion = $relacion; $this->fechaNacimiento = $fechaNacimiento; $this->idEmpleado = $idEmpleado; $this->NombreCompleto = $NombreCompleto;
     }
 
-    public function getIdFamiliar() {
-        return $this->idFamiliar;
-    }
-    public function getNombre() {
-        return $this->nombre;
-    }
-    public function getApellido() {
-        return $this->apellido;
-    }
-    public function getRelacion() {
-        return $this->relacion;
-    }
-    public function getFechaNacimiento() {
-        return $this->fechaNacimiento;
-    }
-    public function getIdEmpleado() {
-        return $this->idEmpleado;
-    }
-    public function getNombreCompleto() {
-        return $this->NombreCompleto;
-    }
-    public function setIdFamiliar($idFamiliar) {
-        $this->idFamiliar = $idFamiliar;
-    }
-    public function setNombre($nombre) {
-        $this->nombre = $nombre;
-    }
-    public function setApellido($apellido) {
-        $this->apellido = $apellido;
-    }
-    public function setRelacion($relacion) {
-        $this->relacion = $relacion;
-    }
-    public function setFechaNacimiento($fechaNacimiento) {
-        $this->fechaNacimiento = $fechaNacimiento;
-    }
-    public function setIdEmpleado($idEmpleado) {
-        $this->idEmpleado = $idEmpleado;
-    }
-    public function setNombreCompleto($NombreCompleto) {
-        return $this->NombreCompleto;
-    }
+    public function getIdFamiliar() { return $this->idFamiliar; }
+    public function getNombre() { return $this->nombre; }
+    public function getApellido() { return $this->apellido; }
+    public function getRelacion() { return $this->relacion; }
+    public function getFechaNacimiento() { return $this->fechaNacimiento; }
+    public function getIdEmpleado() { return $this->idEmpleado; }
+    public function getNombreCompleto() { return $this->NombreCompleto; }
+    
+    public function setIdFamiliar($idFamiliar) { $this->idFamiliar = $idFamiliar; }
+    public function setNombre($nombre) { $this->nombre = $nombre; }
+    public function setApellido($apellido) { $this->apellido = $apellido; }
+    public function setRelacion($relacion) { $this->relacion = $relacion; }
+    public function setFechaNacimiento($fechaNacimiento) { $this->fechaNacimiento = $fechaNacimiento; }
+    public function setIdEmpleado($idEmpleado) { $this->idEmpleado = $idEmpleado; }
+    public function setNombreCompleto($NombreCompleto) { $this->NombreCompleto = $NombreCompleto; }
 }
 ?>
