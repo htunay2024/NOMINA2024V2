6298754de4a8dbcf4f0a58b5f2bd9ec9aa2482a4
htunay2024 <harvytunay2020@gmail.com>
2024-11-01 17:51:41 -0600

    Update Poliza.php

--- Model/Poliza.php
+++ Model/Poliza.php
@@ -8,7 +8,6 @@ class Poliza
     private $ID_Empleado;
     private $NombreCompleto;
     private $CuentaContable;
-
     public function __construct($ID_Poliza, $Fecha, $Descripcion, $Monto, $ID_Empleado, $NombreCompleto=null, $CuentaContable=null, $NombreContable=null)
     {
         $this->ID_Poliza = $ID_Poliza;
@@ -24,67 +23,54 @@ class Poliza
     {
         return $this->ID_Poliza;
     }
-
     public function getFecha()
     {
         return $this->Fecha;
     }
-
     public function getDescripcion()
     {
         return $this->Descripcion;
     }
-
     public function getMonto()
     {
         return $this->Monto;
     }
-
     public function getIDEmpleado()
     {
         return $this->ID_Empleado;
     }
-
     public function getNombreCompleto()
     {
         return $this->NombreCompleto;
     }
-
     public function getCuentaContable()
     {
         return $this->CuentaContable;
     }
-
     public function setIDPoliza($ID_Poliza)
     {
         $this->ID_Poliza = $ID_Poliza;
     }
-
     public function setFecha($Fecha)
     {
         $this->Fecha = $Fecha;
     }
-
     public function setDescripcion($Descripcion)
     {
         $this->Descripcion = $Descripcion;
     }
-
     public function setMonto($Monto)
     {
         $this->Monto = $Monto;
     }
-
     public function setIDEmpleado($ID_Empleado)
     {
         return $this->ID_Empleado = $ID_Empleado;
     }
-
     public function setNombreCompleto($NombreCompleto)
     {
         return $this->NombreCompleto = $NombreCompleto;
     }
-
     public function setCuentaContable($CuentaContable)
     {
         return $this->CuentaContable = $CuentaContable;
