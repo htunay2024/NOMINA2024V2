36d547c2f5b8c08059206423dc144d0fe09c4656
htunay2024 <harvytunay2020@gmail.com>
2024-11-01 17:14:30 -0600

    Update Comisiones.php

--- Model/Comisiones.php
+++ Model/Comisiones.php
@@ -1,5 +1,4 @@
 <?php
-
 class Comisiones
 {
     private $ID_Comision;
@@ -11,7 +10,6 @@ class Comisiones
     private $idEmpleado;
     private $nombreCompleto;
     private $cuentaContable;
-
     public function __construct($ID_Comision, $mes, $anio, $montoVentas, $porcentaje, $comision, $idEmpleado, $nombreCompleto, $cuentaContable) {
         $this->ID_Comision = $ID_Comision;
         $this->mes = $mes;
@@ -23,79 +21,59 @@ class Comisiones
         $this->nombreCompleto = $nombreCompleto;
         $this->cuentaContable = $cuentaContable;
     }
-
-    // Métodos Getters
     public function getIDComision() {
         return $this->ID_Comision;
     }
-
     public function getMes() {
         return $this->mes;
     }
-
     public function getAnio() {
         return $this->anio;
     }
-
     public function getMontoVentas() {
         return $this->montoVentas;
     }
-
     public function getPorcentaje() {
         return $this->porcentaje;
     }
-
     public function getComision() {
         return $this->comision;
     }
-
     public function getIDEmpleado() {
         return $this->idEmpleado;
     }
-
     public function getNombreCompleto() {
         return $this->nombreCompleto;
     }
-
     public function getCuentaContable() {
         return $this->cuentaContable;
     }
 
-    // Métodos Setters
     public function setIDComision($ID_Comision) {
         $this->ID_Comision = $ID_Comision;
     }
-
     public function setMes($mes) {
         $this->mes = $mes;
     }
-
     public function setAnio($anio) {
         $this->anio = $anio;
     }
-
     public function setMontoVentas($montoVentas) {
         $this->montoVentas = $montoVentas;
     }
-
     public function setPorcentaje($porcentaje) {
         $this->porcentaje = $porcentaje;
     }
-
     public function setComision($comision) {
         $this->comision = $comision;
     }
-
     public function setIDEmpleado($idEmpleado) {
         $this->idEmpleado = $idEmpleado;
     }
-
     public function setNombreCompleto($nombreCompleto) {
         return $this->nombreCompleto = $nombreCompleto;
     }
-
     public function setCuentaContable($cuentaContable) {
         return $this->cuentaContable = $cuentaContable;
     }
 }
-
