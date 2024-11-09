0832c6aa65a65575d2326ba63ffcaafb14401591
htunay2024 <harvytunay2020@gmail.com>
2024-11-09 09:58:52 -0600

    Update v.prestamo.php

--- Views/v.prestamo.php
+++ Views/v.prestamo.php
@@ -193,7 +193,6 @@ $prestamos = $prestamoODB->getAll();
     </section>
         <div class="botones-acciones">
             <button onclick="window.location.href='v.nuevo.prestamo.php'" class="btn btn-nuevo">Nuevo Préstamo</button>
-            <button onclick="window.location.href='v.editar.prestamo.php'" class="btn btn-editar">Editar Préstamo</button>
             <button onclick="window.location.href='v.HistorialPagosPrestamos.php'" class="btn btn-historial">Historial de Pagos</button>
         </div>
 </main>
